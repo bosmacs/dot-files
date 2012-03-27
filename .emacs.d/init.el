ae4ad71f484c137ccb2b4226b550232d3e9c99da
Craig S. Bosma <craig.bosma@gmail.com>
2012-03-26 21:40:05 -0500

    Add literate org-mode emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,16 @@
+;;; init.el --- Where all the magic begins
+;;
+;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
+;; embedded in literate Org-mode files.
+
+;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+;; load up Org-mode and Org-babel
+(require 'org-install)
+(require 'ob-tangle)
+
+;; load up all literate org-mode files in this directory
+(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+
+;;; init.el ends here
